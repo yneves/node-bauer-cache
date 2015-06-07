9a53cbcb358e3ae1b10722c03864910f759b17f4
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-06-07 18:13:14 -0300

    tests

--- test/test-cache.js
+++ test/test-cache.js
@@ -2,12 +2,35 @@
 
 "use strict";
 
+var fs = require("fs");
 var assert = require("assert");
 var Cache = require("../");
+
+var options = {
+  json: true,
+  file: {
+    dir: __dirname,
+    ext: "json",
+    name: "test"
+  },
+  expires: 900
+};
 
 // - -------------------------------------------------------------------- - //
 
 describe("Cache",function() {
+  
+  afterEach(function(done) {
+    fs.unlink(__dirname + "/test.json",function() {
+      done();
+    });
+  });
+  
+  beforeEach(function(done) {
+    fs.unlink(__dirname + "/test.json",function() {
+      done();
+    });
+  });
 
   it("constructor error",function() {
     assert.throws(function() {
@@ -16,16 +39,73 @@ describe("Cache",function() {
   });
   
   it("constructor",function() {
-    var cache = new Cache({
-      json: true,
-      file: {
-        dir: __dirname + "/cache",
-        ext: "json",
-        name: "test"
-      },
-      expires: 100
-    });
+    var cache = new Cache(options);
     assert.ok(cache instanceof Cache);
+  });
+  
+  it("exists",function(done) {
+    var cache = new Cache(options);
+    cache.exists(function(error,exists) {
+      assert.strictEqual(error,null);
+      assert.strictEqual(exists,false);
+      done();
+    });
+  });
+  
+  it("write",function(done) {
+    var cache = new Cache(options);
+    cache.write({ test: true },function(error) {
+      assert.strictEqual(error,null);
+      var data = JSON.parse(fs.readFileSync(__dirname + "/test.json","utf8"));
+      assert.deepEqual(data,{ test: true });
+      done();
+    });
+  });
+  
+  it("read",function(done) {
+    var cache = new Cache(options);
+    cache.write({ test: true },function(error) {
+      assert.strictEqual(error,null);
+      cache.read(function(error,data) {
+        assert.strictEqual(error,null);
+        assert.deepEqual(data,{ test: true });
+        done();
+      });
+    });
+  });
+  
+  it("not expired",function(done) {
+    var cache = new Cache(options);
+    cache.write({ test: true },function(error) {
+      assert.strictEqual(error,null);
+      cache.expired(function(error,expired) {
+        assert.strictEqual(error,null);
+        assert.strictEqual(expired,false);
+        done();
+      });
+    });
+  });
+  
+  it("expired error",function(done) {
+    var cache = new Cache(options);
+    cache.expired(function(error) {
+      assert.strictEqual(error.code,"ENOENT");
+      done();
+    });
+  });
+  
+  it("expired",function(done) {
+    var cache = new Cache(options);
+    cache.write({ test: true },function(error) {
+      assert.strictEqual(error,null);
+      setTimeout(function() {
+        cache.expired(function(error,expired) {
+          assert.strictEqual(error,null);
+          assert.strictEqual(expired,true);
+          done();
+        });
+      },options.expires + 100);
+    });
   });
   
 });
